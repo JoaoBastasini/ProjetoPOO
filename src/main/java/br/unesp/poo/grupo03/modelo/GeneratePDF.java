2e5aa0c4e986bd1e0a3fc0cceeab9ffd7ef596c9
JoaoBastasini <joao.bastasini@unesp.br>
2024-06-22 15:58:51 -0300

    Bug fix

--- src/main/java/br/unesp/poo/grupo03/modelo/GeneratePDF.java
+++ src/main/java/br/unesp/poo/grupo03/modelo/GeneratePDF.java
@@ -4,18 +4,17 @@ import com.itextpdf.kernel.geom.PageSize;
 import com.itextpdf.kernel.pdf.PdfDocument;
 import com.itextpdf.kernel.pdf.PdfWriter;
 import com.itextpdf.layout.Document;
-import com.itextpdf.layout.element.Paragraph;
-import static java.awt.SystemColor.text;
 import java.io.FileNotFoundException;
 
 
 public class GeneratePDF {
-     public static void main(String[] args) throws FileNotFoundException{
-         String path = "Dieta.pdf";
+     public static void geradorPDF() throws FileNotFoundException{
+         String path = "C:\\Users\\Joao Bastasini\\OneDrive\\Documentos\\NetBeansProjects\\ProjectPOO\\Dieta.pdf";
          PdfWriter pdfWriter = new PdfWriter(path);
          PdfDocument pdfDocument = new PdfDocument(pdfWriter);
          pdfDocument.setDefaultPageSize(PageSize.A4);
          Document document = new Document(pdfDocument);
+         
          
          document.close();
      }
